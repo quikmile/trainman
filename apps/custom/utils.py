f4a7ca60cbf604f5fddb8d7145437403827d0b1e
Abhishek Verma <ashuverma1989@gmail.com>
2017-03-01 15:51:51 +0530

    update rando string function to lowercase

--- apps/custom/utils.py
+++ apps/custom/utils.py
@@ -3,7 +3,8 @@ import string
 
 
 def get_random_string(length):
-    return ''.join(random.choice(string.ascii_uppercase + string.lowercase + string.digits) for _ in range(length))
+    return ''.join(
+        random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
 
 
 def remove_quotes(string):
