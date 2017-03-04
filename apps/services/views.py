f7f61df079e7664a5856e8495aaab7c5a3fa022a
Abhishek Verma <ashuverma1989@gmail.com>
2017-03-04 13:30:19 +0530

    @csrf_exempt added on webhook

--- apps/services/views.py
+++ apps/services/views.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 import json
 
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Service
 
 
+@csrf_exempt
 def webhook_trigger(request):
     response = json.loads(request.body)
     print request.META.get('X-Gitlab-Event')
