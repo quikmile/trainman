7edbe08a87a0f49a8afdc2b366daa6327eccbbdd
Abhishek Verma <ashuverma1989@gmail.com>
2017-03-04 13:38:38 +0530

    fixed  deployment webhook issue

--- apps/services/views.py
+++ apps/services/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import json
 
+from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Service
@@ -11,8 +12,9 @@ from .models import Service
 @csrf_exempt
 def webhook_trigger(request):
     response = json.loads(request.body)
-    print request.META.get('X-Gitlab-Event')
-    print response
-    if response['object_kind'] == 'tag_push':
+    print request.META
+    if response.get('object_kind') == 'tag_push':
         service = Service.objects.get(gitlab_project_id=response['project_id'])
         service.deploy()
+
+    return HttpResponse(status=200)
