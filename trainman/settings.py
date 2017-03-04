8adc050151a6b9be99eee127402465c63958c906
Abhishek Verma <ashuverma1989@gmail.com>
2017-03-04 16:59:47 +0530

    added service admin

--- trainman/settings.py
+++ trainman/settings.py
@@ -180,7 +180,7 @@ SLACK_BACKEND = 'django_slack.backends.CeleryBackend'
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse',
