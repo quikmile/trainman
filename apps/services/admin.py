29bcd641f9a830fcfe6a457021632646f5809b77
abhishek <ashuverma1989@gmail.com>
2017-07-15 03:19:24 +0530

    added admin autoregister

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,28 +1,28 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
-#
-# from django.contrib import admin
-#
-# from .models import *
-#
-#
-# class ServiceAdmin(admin.ModelAdmin):
-#     list_display = ('service_name', 'service_uri', 'content_object', 'database_id', 'contributors', 'service_registry')
-#     list_filter = ['service_registry', 'is_active']
-#
-#
-# admin.site.register(Service, ServiceAdmin)
-# admin.site.register(ServiceNode)
-# admin.site.register(ServiceNodeType)
-# admin.site.register(ServiceRegistryNode)
-# admin.site.register(ServiceInstance)
-# admin.site.register(TrellioAdmin)
-import os
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from django.apps import apps
+from django.contrib import admin
 
-from ..custom.utils import register_all_models
+from .models import *
 
-app_label = os.path.abspath(__file__).split('/')[-2]
-app = apps.get_app_config(app_label)
-register_all_models(models=app.models, database=app_label)
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_name', 'service_uri', 'content_object', 'database_id', 'contributors', 'service_registry')
+    list_filter = ['service_registry', 'is_active']
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(ServiceNode)
+admin.site.register(ServiceNodeType)
+admin.site.register(ServiceRegistryNode)
+admin.site.register(ServiceInstance)
+admin.site.register(TrellioAdmin)
+# import os
+#
+# from django.apps import apps
+#
+# from ..custom.utils import register_all_models
+#
+# app_label = os.path.abspath(__file__).split('/')[-2]
+# app = apps.get_app_config(app_label)
+# register_all_models(models=app.models, database=app_label)
