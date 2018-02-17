a94d57118463e0331c92e2e75cb863ba796144b0
Abhishek Verma <ashuverma1989@gmail.com>
2018-02-17 19:54:37 +0530

    updated webhook

--- apps/services/views.py
+++ apps/services/views.py
@@ -6,6 +6,7 @@ import json
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from apps.services.models import ServiceNode
 from ..base.utils import json_response
 from .models import Service
 
@@ -13,18 +14,18 @@ from .models import Service
 @csrf_exempt
 def webhook_trigger(request):
     response = json.loads(request.body)
-    if response.get('object_kind') == 'tag_push':
-        service = Service.objects.get(gitlab_project_id=response['project_id'])
+    if response.get('object_kind') == 'push' and response.get('ref') == 'refs/heads/master':
+        service = Service.objects.get(gitlab_project_id=response['project_id'], servicenodetype__server_type='STAG')
         service.deploy()
 
     return HttpResponse(status=200)
 
 
-def get_service_uri(request):
-    service_name = request.GET.get('name')
-    service_version = request.GET.get('version')
-    if not service_name and not service_version:
-        return json_response({"error": "GET params name and version cannot null"}, status=400)
-
-    service = Service.get_service_by_name_version(service_name, service_version)
-    return json_response(service.service_uri, status=200)
+# def get_service_uri(request):
+#     service_name = request.GET.get('name')
+#     service_version = request.GET.get('version')
+#     if not service_name and not service_version:
+#         return json_response({"error": "GET params name and version cannot null"}, status=400)
+#
+#     service = ServiceNode.get_service_by_name_version(service_name, service_version)
+#     return json_response(service.service_uri, status=200)
